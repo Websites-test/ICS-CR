aa0444c47b51568fc4a0b4047398f82d36101880
Websites-test <melissarosalesmora@gmail.com>
2018-01-31 12:51:40 -0600

    cambio form

--- enviar.php
+++ enviar.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
 
 // Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
-$email_to = "digital@seppublicidad.com";
+$email_to = "m507@live.com.mx";
 $email_subject = "Contacto desde el sitio web";
 
 // Aquí se deberían validar los datos ingresados por el usuario
@@ -26,9 +26,9 @@ $email_message .= "Comentarios: " . $_POST['comments'] . "\n\n";
 
 
 // Ahora se envía el e-mail usando la función mail() de PHP
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion(7.2.1);
+$headers = 'From: '.$email_to."\r\n".
+'Reply-To: '.$email_to."\r\n" .
+'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
 
 echo "¡El formulario se ha enviado con éxito!";
